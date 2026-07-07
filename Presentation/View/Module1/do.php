2e533be3f038b43339e0726ffe8c4b9523c98ec0
AkmaDiani <akmadiani6@gmail.com>
2026-07-08 00:50:06 +0800

    Update do.php

--- Presentation/View/Module1/do.php
+++ Presentation/View/Module1/do.php
@@ -3,6 +3,8 @@ $title = 'My Delivery Orders - KTM eDOIS';
 $showTopbar = true;
 $showSidebar = true;
 include ROOT_PATH . '/Presentation/View/SharedUI/topbar.php';
+include ROOT_PATH . '/Presentation/View/SharedUI/sidebarM1.php';
+
 ?>
 
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
