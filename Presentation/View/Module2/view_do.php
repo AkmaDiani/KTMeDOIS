bf8bd68710b82c7549b70ce67d051b51dc14a8bc
AkmaDiani <akmadiani6@gmail.com>
2026-07-07 21:14:00 +0800

    Update view_do.php

--- Presentation/View/Module2/view_do.php
+++ Presentation/View/Module2/view_do.php
@@ -1,6 +1,5 @@
 <?php
-require_once __DIR__ . "/../../Application/Model/db.php";
-require_once __DIR__ . "/../../Application/Controllers/DOService.php";
+require_once __DIR__ . '/../../bootstrap.php';
 
 session_start();
 
