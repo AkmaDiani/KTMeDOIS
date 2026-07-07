a2dec5678da454b6c1987b72b7fa344bccf4088f
AkmaDiani <akmadiani6@gmail.com>
2026-07-07 18:41:58 +0800

    Create do.php

---
+++ Presentation/View/Module1/do.php
@@ -0,0 +1,49 @@
+<?php
+$title = 'My Delivery Orders - KTM eDOIS';
+$showTopbar = true;
+$showSidebar = true;
+include __DIR__ . '/../SharedUI/topbar.php';
+?>
+
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">My Delivery Orders</h1>
+    <span class="badge bg-primary">Total: <?php echo count($doList); ?></span>
+</div>
+
+<div class="card card-ktm">
+    <div class="card-header bg-primary text-white"><i class="fas fa-list"></i> Delivery Orders</div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>DO Number</th>
+                        <th>PO Number</th>
+                        <th>Status</th>
+                        <th>Created By</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if (empty($doList)): ?>
+                        <tr>
+                            <td colspan="5" class="text-center text-muted py-4"><i class="fas fa-truck fa-2x d-block mb-2"></i> No delivery orders found.</td>
+                        </tr>
+                    <?php else: ?>
+                        <?php foreach ($doList as $row): ?>
+                            <tr>
+                                <td><?php echo $row['DO_number']; ?></td>
+                                <td><?php echo $row['PO_number']; ?></td>
+                                <td><?php echo getStatusBadge($row['Status']); ?></td>
+                                <td><?php echo $row['created_by']; ?></td>
+                                <td><?php echo date('d/m/Y', strtotime($row['created_date'])); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . '/../SharedUI/footer.php'; ?>
